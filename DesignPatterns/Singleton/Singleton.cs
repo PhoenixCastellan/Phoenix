72eab53c2ac3167fbf06a3c4bac43a791f5e8224
吕日辉 <lv.rihui@ronghere.com>
2017-08-21 16:24:52 +0800

    添加了单例模式的例子

---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _singleton;
+        private static readonly object _obj = new object();
+
+        /// <summary>
+        /// 线程不安全
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetInstance()
+        {
+            if (_singleton == null)
+            {
+                _singleton=new Singleton();
+            }
+            return _singleton;
+        }
+
+        /// <summary>
+        /// 线程不安全
+        /// A、B两个线程，A和B都执行到Lock，然后A执行完了之后，B继续执行的时候会再生成一个对象
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetInstanceV1()
+        {
+            if (_singleton != null) return _singleton;
+            lock (_obj)
+            {
+                _singleton = new Singleton();
+            }
+            return _singleton;
+        }
+
+        public static Singleton GetInstanceV2()
+        {
+            if (_singleton != null) return _singleton;
+            lock (_obj)
+            {
+                if (_singleton == null)
+                {
+                    _singleton = new Singleton();
+                }
+            }
+            return _singleton;
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/SingletonV2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class SingletonV2
+    {
+        private SingletonV2() { }
+        private static object _lock=new object();
+
+        private static SingletonV2 _singleton;
+
+        public static SingletonV2 GetInstance()
+        {
+            if (_singleton == null)
+            {
+                lock (_lock)
+                {
+                    _singleton = new SingletonV2();
+                }
+            }
+            return _singleton;
+        }
+    }
+}
